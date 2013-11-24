dbc8962a5160301f665c4a54c94923f56b6e15c1
Vipul A M <vipulnsward@gmail.com>
2013-11-24 23:37:28 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DribbleGifs::Application.config.secret_key_base = '46e1fa48f6dd1ea10d61709bb1d5bfd38fd8fd57b687f412f7ac87240a79b541b99895e421b3eb0738e0fd02a59dc22129927846d983532ea8fd7f0011983e81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DribbleGifs::Application.config.session_store :cookie_store, key: '_dribble-gifs_session'
